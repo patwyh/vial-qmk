f392c10d8bdd0390ef32c05f2692e8a0782655c0
ergokeeb <patwyh_reg@yahoo.com.hk>
2025-10-08 17:43:09 +0800

    add cirque trackpad
    8 oct 2025

--- keyboards/crkbd/keymaps/vial/rules.mk
+++ keyboards/crkbd/keymaps/vial/rules.mk
@@ -15,3 +15,16 @@ QMK_SETTINGS        = no
 CAPS_WORD_ENABLE = no
 LAYER_LOCK_ENABLE = no
 REPEAT_KEY_ENABLE = no
+
+POINTING_DEVICE_ENABLE = yes
+POINTING_DEVICE_DRIVER = cirque_pinnacle_spi
+CIRQUE_PINNACLE_DIAMETER_MM = 23  # Adjust to your trackpad size: 23, 35, or 40
+#CIRQUE_PINNACLE_TAP_ENABLE        # Enables tap-to-click
+#POINTING_DEVICE_GESTURES_SCROLL_ENABLE  # Enables scroll gestures
+
+#CIRQUE_PINNACLE_SPI_CS_PIN = POINTING_DEVICE_CS_PIN
+
+
+
+# Throttle pointing device updates for performance (optional, 10ms = 100Hz)
+#POINTING_DEVICE_TASK_THROTTLE_MS = 10
