8cee5b21b65754b62ed95bb76b365d92f7dc5389
ergokeeb <patwyh_reg@yahoo.com.hk>
2026-06-02 00:08:10 +0800

    temp

---
+++ keyboards/ergokeeb/ekrp_test/config.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#define SERIAL_USART_TX_PIN GP12
+#define USB_VBUS_PIN GP11
+
+// /* USB Device descriptor parameter */
+// #define VENDOR_ID 0x7111
+// #define PRODUCT_ID 0x6224
+// #define DEVICE_VER 0x0001
+// #define MANUFACTURER "ergokeeb"
+// #define PRODUCT "trackpadtest"
+
+
+// Configure the RP2040 SPI Driver
+#define SPI_DRIVER SPID0
+#define SPI_MISO_PIN GP16  // RX
+#define SPI_SCK_PIN  GP18  // SCK
+#define SPI_MOSI_PIN GP19  // TX
+
+// Configure the Cirque Trackpad
+#define POINTING_DEVICE_CS_PIN GP15
+#define CIRQUE_PINNACLE_SPI_CS_PIN GP15
+
+// Trackpad Physical Dimensions (Change to 40 if you have the larger one)
+#define CIRQUE_PINNACLE_DIAMETER_MM 23
+//#define CIRQUE_PINNACLE_CURVED_OVERLAY
+#define CIRQUE_DEVICE_GESTURES_SCROLL_ENABLE // Circular scroll.
+#define CIRQUE_PINNACLE_TAP_ENABLE_RIGHT
+
+#define SPLIT_POINTING_ENABLE
+#define POINTING_DEVICE_COMBINED
+#define POINTING_DEVICE_ROTATION_180_RIGHT // Uncomment if your ribbon cable exits to the right
---
+++ keyboards/ergokeeb/rp2040_test/config.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#define SERIAL_USART_TX_PIN GP12
+#define USB_VBUS_PIN GP11
+
